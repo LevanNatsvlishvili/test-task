461012022cc8fa9ccd8672b679b7abb8513f90c9
Levan Natsvlishvili <L.nats51@yahoo.com>
2024-05-29 21:37:28 +0400

    feat: added boxes, shadows and hover animation

--- src/pages/DragAndDropAnime/DragAndDropAnime.tsx
+++ src/pages/DragAndDropAnime/DragAndDropAnime.tsx
@@ -1,14 +1,55 @@
-import { Suspense, useEffect, useRef, useState, useTransition } from 'react';
+import { Suspense, useEffect, useRef, useState } from 'react';
 import { Canvas, useLoader } from '@react-three/fiber';
 import { TextureLoader } from 'three';
-import { Html } from '@react-three/drei';
 import { gsap } from 'gsap';
-import { texture } from 'three/examples/jsm/nodes/Nodes.js';
 
-const ImagePlane = ({ texture, position }) => {
+const Model = ({ path }) => {
+  const texture = useLoader(TextureLoader, path);
+  return <meshStandardMaterial map={texture} transparent castShadow receiveShadow />;
+};
+
+const Lights = () => {
+  return (
+    <>
+      <ambientLight intensity={2} />
+      <directionalLight position={[10, 15, 500]} intensity={1} castShadow />
+    </>
+  );
+};
+
+const ImagePlane = ({ texture, shadowTexture, position, index, hovered, animation }) => {
   const ref = useRef();
+
   useEffect(() => {
-    // Animate the scale for a smooth appearance
+    if (ref.current) {
+      const rotationAnim = gsap.to(ref.current.rotation, {
+        z: hovered && animation.rotate !== 0 ? Math.PI / animation.rotate : 0,
+        duration: 1,
+        ease: 'power3.out',
+      });
+      const positionAnim = gsap.to(ref.current.position, {
+        x: hovered && animation.x !== 0 ? position[0] + animation.x : position[0], // Move slightly to the left
+        y: hovered && animation.y !== 0 ? position[1] + animation.y : position[1], // Move slightly up
+        duration: 1,
+        ease: 'power3.out',
+      });
+      const scaleAnim = gsap.to(ref.current.scale, {
+        x: hovered && index === 1 ? 1.2 : 1,
+        y: hovered && index === 1 ? 1.2 : 1,
+        z: hovered && index === 1 ? 1.2 : 1,
+        duration: 1,
+        ease: 'power3.out',
+      });
+
+      return () => {
+        rotationAnim.kill();
+        positionAnim.kill();
+        scaleAnim.kill();
+      };
+    }
+  }, [hovered]);
+
+  useEffect(() => {
     if (ref.current) {
       ref.current.scale.set(0, 0, 0);
       gsap.to(ref.current.scale, {
@@ -22,57 +63,86 @@ const ImagePlane = ({ texture, position }) => {
   }, [texture]);
 
   return (
-    <mesh position={position} ref={ref}>
-      <planeGeometry args={[60, 50]} />
-      <meshBasicMaterial map={texture} transparent />
-    </mesh>
+    <group ref={ref} position={position}>
+      <mesh position={[0, -5 * (index + 1), 0]}>
+        <planeGeometry args={[80, 70]} />
+        <Model path={shadowTexture} />
+      </mesh>
+      <mesh>
+        <planeGeometry args={[60, 50]} />
+        <Model path={texture} />
+      </mesh>
+    </group>
+  );
+};
+
+const GroupMesh = ({ boxes }) => {
+  const [hovered, setHovered] = useState(false);
+  const handleOnHover = () => {
+    setHovered((prev) => !prev);
+  };
+  return (
+    <>
+      <group onPointerOver={handleOnHover} onPointerOut={handleOnHover}>
+        <Lights />
+
+        {boxes.map((box, index) => (
+          <ImagePlane
+            key={index}
+            texture={box.texturePath}
+            shadowTexture={box.shadowTexture}
+            position={box.position}
+            index={index}
+            hovered={hovered}
+            animation={box.animation}
+          />
+        ))}
+      </group>
+    </>
   );
 };
 
 const boxes = [
-  { texturePath: '/images/products/product-1.png', title: 'Blue', bgColor: '#f15945', position: [-23, 5, -10] },
-  { texturePath: '/images/products/product-2.png', title: 'Grey', bgColor: '#571ec1', position: [0, 0, 0] },
-  { texturePath: '/images/products/product-3.png', title: 'Pink', bgColor: '#F2C649', position: [18, -5, 10] },
+  {
+    texturePath: '/images/products/product-1.png',
+    shadowTexture: '/images/products/product-1-shadow.png',
+    position: [-23, 5, -10],
+    animation: {
+      rotate: -5,
+      x: -15,
+      y: 5,
+    },
+  },
+  {
+    texturePath: '/images/products/product-2.png',
+    shadowTexture: '/images/products/product-2-shadow.png',
+    position: [0, 0, 0],
+    animation: {
+      rotate: 0,
+      x: -2.5,
+      y: -2.5,
+    },
+  },
+  {
+    texturePath: '/images/products/product-3.png',
+    shadowTexture: '/images/products/product-3-shadow.png',
+    position: [18, -5, 10],
+    animation: {
+      rotate: -5,
+      x: 15,
+      y: -5,
+    },
+  },
 ];
 
 const DragAndDropAnime = () => {
-  const textures = boxes.map((box) => ({
-    model: useLoader(TextureLoader, box.texturePath),
-    position: box.position,
-  }));
-  const [currentTexture, setCurrentTexture] = useState(textures[0]);
-  const [isPending, startTransition] = useTransition();
-
-  const handleTextureChange = (index) => {
-    startTransition(() => {
-      setCurrentTexture(textures[index]);
-    });
-  };
-
-  useEffect(() => {
-    // Update the texture if the textures array changes
-    setCurrentTexture(textures[0]);
-  }, [textures]);
-
   return (
     <div className="h-100vh w-full">
-      <Canvas camera={{ position: [0, 0, 120], fov: 70 }}>
+      <Canvas shadows camera={{ position: [0, 0, 120], fov: 70 }}>
         <Suspense fallback={null}>
-          {textures.map((texture, index) => (
-            <ImagePlane key={index} texture={texture.model} position={texture.position} position={texture.position} />
-          ))}
-
-          {/* Lights Component */}
-          {/* <Lights /> */}
+          <GroupMesh boxes={boxes} />
         </Suspense>
       </Canvas>
-      {/* <div>
-        {boxes.map((box, index) => (
-          <button key={box.texturePath} onClick={() => handleTextureChange(index)}>
-            {box.title}
-          </button>
-        ))}
-      </div> */}
     </div>
   );
 };
